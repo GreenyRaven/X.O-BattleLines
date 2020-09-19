db1ac067231cca54edcf642f60dc1e61e879a0cc
VirtuallInsanity <48131753+VirtuallInsanity@users.noreply.github.com>
2020-09-19 19:10:32 +0400

    first commit

---
+++ src/Model/tictactoe.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Array;
+
+public class tictactoe {
+    public static void main(String[] args) {
+        System.out.println("start");
+        int[][] board = Create_board();
+        System.out.println(board[0][0]);
+        System.out.println("end");
+    }
+    public static int[][] Create_board(){
+        int[][] arr = new int[3][3];
+        return arr;
+    }
+
+    public static void Place(int[][] board, int player, Array  position){
+
+    }
+
+}
